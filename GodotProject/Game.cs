d3af42a177bd5b36f72739c869ca6667bddcb504
Peter S <peter@anyline.io>
2021-04-24 17:35:12 +0200

    oh gott...

--- GodotProject/Game.cs
+++ GodotProject/Game.cs
@@ -7,10 +7,12 @@ public class Game : Node2D
 	// private int a = 2;
 	// private string b = "text";
 
+	private Node player;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Node map = GetNode("Map");
+		player = GetNode("Player");
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
